96f3ce1769bc40535fb6a72cc5f21444004c270e
Grando <guigrando2@gmail.com>
2023-01-03 14:15:49 -0300

    envio do projeto

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,21 @@
+import OpcoesHeader from '../opcoesHeader'
+import Logo from   '../Logo'
+import IconesHeader from '../IconesHeader'
+import style from 'styled-components'
+
+const HeaderContainer = style.header `
+  background-color: #FFF;
+  display: flex;
+  justify-content: center;
+`
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <Logo />
+      <OpcoesHeader />
+      <IconesHeader />
+    </HeaderContainer>
+  )
+}
+export default Header;
