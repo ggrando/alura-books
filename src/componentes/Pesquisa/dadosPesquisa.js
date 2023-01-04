a6321687ed4d2462014e8b8b55095af15680c5f1
Grando <guigrando2@gmail.com>
2023-01-04 16:11:55 -0300

    eventos

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+  { nome: 'Liderança em design', id: 1, src: livro },
+  { nome: 'Curso de React', id: 2, src: livro },
+  { nome: 'React curso avançado', id: 3, src: livro },
+  { nome: 'Aventureiros do React', id: 4, src: livro },
+  { nome: 'Use a cabeça', id: 5, src: livro },
+  { nome: 'fonte de dados', id: 6, src: livro },
+]
