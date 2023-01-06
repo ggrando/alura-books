4538531a706d787aec49592af47552fcdf51ad50
Grando <guigrando2@gmail.com>
2023-01-06 19:16:27 -0300

    Trabalhando com props

--- src/componentes/UltimosLancamentos/index.jsx
+++ src/componentes/UltimosLancamentos/index.jsx
@@ -1,5 +1,6 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
+import { Titulo } from '../Titulo'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -8,15 +9,7 @@ const UltimosLancamentosContainer = styled.section`
     flex-direction: column;
 `
 
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
-`
+
 
 const NovosLivrosContainer = styled.div`
     margin-top: 30px;
@@ -29,11 +22,15 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
   return (
     <UltimosLancamentosContainer>
-      <Titulo>Últimos Lancamentos</Titulo>
+      <Titulo
+        cor="#EB9B00"
+        tamanhoFonte="36px"
+        alinhamento="center"
+      > Últimos Lançamentos</Titulo>
       <NovosLivrosContainer>
         {
           livros.map(livro => (
-            <img src={livro.src} />
+            <img src={livro.src} alt={livro.nome} />
           ))}
       </NovosLivrosContainer>
     </UltimosLancamentosContainer>
